843bf2d744ea1431de6453d98c79a9621ce60144
Andrew Crisp <andrew.crisp@gmail.com>
2022-11-06 19:45:19 -0700

    Correct drawing logic for top/bottom and simplify generation order

--- depth_first_recursive_maze_generator/app.py
+++ depth_first_recursive_maze_generator/app.py
@@ -3,7 +3,7 @@ import pygame
 from colors import *
 from maze import Maze
 
-maze_width = 64
+maze_width = 128
 maze_height = 48
 maze = Maze(maze_width, maze_height)
 maze.Generate()
@@ -11,16 +11,17 @@ maze.Generate()
 pygame.init()
 cell_width = 10
 cell_height = 10
-size = (maze_width * cell_width, maze_height * cell_height) # = (64, 48)
+size = (maze_width * cell_width, maze_height * cell_height)
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+
 while True:
-#for i in range(6):
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    
+    screen.fill(black)
+    #maze.Generate_Step()
     maze.Draw(screen, cell_width, cell_height)
     pygame.display.flip()
-    clock.tick(1)
+    clock.tick(60)
     
--- depth_first_recursive_maze_generator/maze_cell.py
+++ depth_first_recursive_maze_generator/maze_cell.py
@@ -11,10 +11,11 @@ class Maze_Cell:
                      'bottom' : True,
                      'left' : True}
         self.visited = False
-        self.neighbors = {'top'   : self.index - maze_width,
-                         'right'  : self.index + 1,
-                         'bottom' : self.index + maze_width,
-                         'left'   : self.index - 1}
+        self.neighbors = {'top'   : (x + ((y - 1) * maze_width)),
+                         'right'  : ((x + 1) + (y * maze_width)),
+                         'bottom' : (x + ((y + 1) * maze_width)),
+                         'left'   : ((x - 1) + (y * maze_width))
+                         }
         if self.x == 0:
             del self.neighbors['left']
         if self.x == (maze_width - 1):
@@ -23,9 +24,6 @@ class Maze_Cell:
             del self.neighbors['top']
         if self.y == (maze_height - 1):
             del self.neighbors['bottom']
-        for n in self.neighbors.keys():
-            if self.neighbors[n] < 0 or self.neighbors[n] > (maze_width * maze_height):
-                del self.neighbors[n]
 
     def Open_Wall(self, neighbor):
         if self.index == neighbor.index + 1:
@@ -40,20 +38,24 @@ class Maze_Cell:
     def Show(self, surface, cell_width, cell_height):
         x = self.x * cell_width
         y = self.y * cell_height
+        tl = (x, y)
+        tr = (x + cell_width, y)
+        br = (x + cell_width, y + cell_height)
+        bl = (x, y + cell_height)
+
         if self.visited:
-            surface.fill(mountbatten_pink,
-                pygame.Rect((x, y), (x + cell_width, y + cell_height)))
+            surface.fill(mountbatten_pink, pygame.Rect((x, y), (cell_width, cell_height)))
         if self.walls['top']:
-            pygame.draw.line(surface, white, pygame.math.Vector2(x, y), pygame.math.Vector2(x + cell_width, y))
+            pygame.draw.line(surface, white, tl, tr)
         if self.walls['right']:
-            pygame.draw.line(surface, white, pygame.math.Vector2(x + cell_width, y), pygame.math.Vector2(x + cell_width, y + cell_height))
+            pygame.draw.line(surface, white, tr, br)
         if self.walls['bottom']:
-            pygame.draw.line(surface, white, pygame.math.Vector2(x, y + cell_height), pygame.math.Vector2(x + cell_width, y + cell_height))
+            pygame.draw.line(surface, white, br, bl)
         if self.walls['left']:
-            pygame.draw.line(surface, white, pygame.math.Vector2(x, y), pygame.math.Vector2(x, y + cell_height))
+            pygame.draw.line(surface, white, tl, bl)
 
     def Highlight(self, surface, cell_width, cell_height):
         x = self.x * cell_width
         y = self.y * cell_height
         surface.fill(turquoise,
-            pygame.Rect((x, y), (x + cell_width, y + cell_height)))
+            pygame.Rect((x, y), (cell_width, cell_height)))
